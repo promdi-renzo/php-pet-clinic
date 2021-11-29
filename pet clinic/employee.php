9e191d81d2bb6c05cb93612286197970a41a8986
renzothenoob <72588010+renzothenoob@users.noreply.github.com>
2021-11-29 12:27:46 +0800

    Fix navbar issues

--- pet clinic/employee.php
+++ pet clinic/employee.php
@@ -3,9 +3,55 @@ require_once('./includes/meta.php')
 ?>
 
 <body>
-    <?php
-    require_once('./components/header.php')
-    ?>
+    <div class="employee-container">
+        <header>
+            <div class="logo">
+                <h1>今日は</h1>
+            </div>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Pet</li>
+                    <li class="selected">Employee</li>
+                    <li>Grooming Services</li>
+                    <li>Transaction</li>
+                </ul>
+            </nav>
+
+            <button>Contact Us</button>
+        </header>
+
+        <div class="employee">
+            <div class="employee-crud">
+                <div class="employee-crud__head">
+                    <h1>Employee</h1>
+                    <button>Add new employee</button>
+                </div>
+                <table class="employee-crud__table">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Phone Number</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Dom</td>
+                            <td>6000</td>
+                        </tr>
+                        <tr class="active-row">
+                            <td>Melissa</td>
+                            <td>5150</td>
+                            <td>Melissa</td>
+                            <td><button>View</button><button>Edit</button><button>Delete</button></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 
 </body>
 
--- pet clinic/home.php
+++ pet clinic/home.php
@@ -3,16 +3,48 @@ require_once('./includes/meta.php')
 ?>
 
 <body>
-    <?php
-    require_once('./components/header.php')
-    ?>
+    <div class="home-container">
+        <header>
+            <div class="logo">
+                <h1>今日は</h1>
+            </div>
+            <nav>
+                <ul>
+                    <li class="selected">Home</li>
+                    <li>Pet</li>
+                    <li>Employee</li>
+                    <li>Grooming Services</li>
+                    <li>Transaction</li>
+                </ul>
+            </nav>
 
-    <div class="home">
-        <div class="home__hero">
-            <h1>Pet Clinic, よこぞ</h1>
+            <button>Contact Us</button>
+        </header>
+
+        <div class="home">
+            <div class="home__hero">
+                <h1>Pet Clinic, よこぞ</h1>
+                <div class="home__hero-content">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                        Eius, iure quos. Eos voluptates sint qui.
+                        Consectetur consequuntur qui quo eveniet voluptates,
+                        tium aspernatur, ex nemo minus magni labore laudantium tenetur.
+                    </p>
+
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                        Eius, iure quos. Eos voluptates sint qui.
+                        Consectetur consequuntur qui quo eveniet voluptates,
+                        tium aspernatur, ex nemo minus magni labore laudantium tenetur.
+                    </p>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                        Eius, iure quos. Eos voluptates sint qui.
+                        Consectetur consequuntur qui quo eveniet voluptates,
+                        tium aspernatur, ex nemo minus magni labore laudantium tenetur.
+                    </p>
+                </div>
+
+            </div>
         </div>
-    </div>
-
     </div>
 
 </body>
