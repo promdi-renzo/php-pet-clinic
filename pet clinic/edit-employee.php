79bc33eee95568e8d747585938a6f60eb4a4dd25
renzothenoob <72588010+renzothenoob@users.noreply.github.com>
2021-11-29 14:00:49 +0800

    Add edit-employee

---
+++ pet clinic/edit-employee.php
@@ -0,0 +1,34 @@
+<?php
+require_once('./includes/meta.php')
+?>
+
+<body>
+    <div class="employee-form-container">
+        <div class="employee-form">
+            <div class="employee-form__logo">
+                <h1>Edit Employee</h1>
+            </div>
+            <form method="post" action="" class="employee-form__form">
+                <div class="employee-form__input">
+                    <label for="id">ID</label>
+                    <input type="text" name="id" id="id" disabled>
+                </div>
+                <div class="employee-form__input">
+                    <label for="usn">Name</label>
+                    <input type="text" name="usn" id="usn">
+                </div>
+                <div class="employee-form__input">
+                    <label for="num">Phone Number</label>
+                    <input type="text" name="num" id="num">
+                </div>
+                <div class="employee-form__input">
+                    <label for="psw">Password</label>
+                    <input type="password" name="psw" id="psw">
+                </div>
+                <button type="submit">Update Employee</button>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
