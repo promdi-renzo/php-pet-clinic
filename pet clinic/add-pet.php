968f419981aee2aa64910e7a3731ed50c7072386
renzothenoob <72588010+renzothenoob@users.noreply.github.com>
2021-11-29 14:17:04 +0800

    Add pet forms

---
+++ pet clinic/add-pet.php
@@ -0,0 +1,34 @@
+<?php
+require_once('./includes/meta.php')
+?>
+
+<body>
+    <div class="employee-form-container">
+        <div class="employee-form">
+            <div class="employee-form__logo">
+                <h1>Add Employee</h1>
+            </div>
+            <form method="post" action="" class="employee-form__form">
+                <div class="employee-form__input">
+                    <label for="breed">Breed</label>
+                    <input type="text" name="breed" id="breed">
+                </div>
+                <div class="employee-form__input">
+                    <label for="gender">Gender</label>
+                    <input type="text" name="gender" id="gender">
+                </div>
+                <div class="employee-form__input">
+                    <label for="age">Age</label>
+                    <input type="text" name="age" id="age">
+                </div>
+                <div class="pet-form__input">
+                    <label for="fileToUpload">Picture</label>
+                    <input type="file" name="fileToUpload" id="fileToUpload">
+                </div>
+                <button type="submit">Add Employee</button>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
